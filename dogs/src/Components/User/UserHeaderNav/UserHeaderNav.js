10e47d442065981bfdb2bc1986a690c212f7a650
Erika Skarda <erikaskarda@yahoo.com.br>
2021-07-06 19:51:48 -0300

    Feat: PhotoPost

--- dogs/src/Components/User/UserHeaderNav/UserHeaderNav.js
+++ dogs/src/Components/User/UserHeaderNav/UserHeaderNav.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import { useUser } from '../../../UseContext/UserContext';
+import { useMedia } from '../../../Hooks/useMedia';
 import { ReactComponent as MinhasFotos } from  '../../../Assets/feed.svg';
 import { ReactComponent as Estatísticas } from  '../../../Assets/estatisticas.svg';
 import { ReactComponent as AdicionarFotos } from  '../../../Assets/adicionar.svg';
@@ -9,27 +10,48 @@ import styles from './UserHeaderNav.module.css';
 
 const UserHeaderNav = () => {
   const { userLogout } = useUser();
-  const [mobile, setMobile] = useState(null);
-  return (
-      <nav className={styles.nav}>
-        <NavLink to="/conta" end activeClassName={styles.active}>
-          <MinhasFotos />
-            {mobile && 'Minhas Fotos'}
-        </NavLink>
-        <NavLink to="/conta/estatisticas" activeClassName={styles.active}>
-          <Estatísticas/>
-            {mobile && 'Estatísticas'}
-        </NavLink>
-        <NavLink to="/conta/postar" activeClassName={styles.active}>
-          <AdicionarFotos />
-          {mobile && 'Adicionar Fotos'}        
-        </NavLink>
+  const mobile = useMedia('(max-width: 40rem)');
+  const [mobileMenu, setMobileMenu] = useState(false);
+  
+  const { pathname} = useLocation();
 
-        <button onClick={userLogout}>
-          <Sair />  
-          {mobile && 'Sair'} 
-        </button>
-      </nav>
+  useEffect(() => {
+    setMobileMenu(false)
+  }, [pathname])
+  return (
+      <>
+        {mobile && 
+          <button 
+            aria-label="Menu" 
+            className={`${styles.mobileButton} 
+            ${mobileMenu && styles.mobileButtonActive}`}
+            onClick={() => setMobileMenu(!mobileMenu)}
+          ></button>
+        }
+        
+        <nav 
+        className={`${mobile ? styles.navMobile : styles.nav}
+        ${mobileMenu && styles.navMobileActive }
+        `}>
+          <NavLink to="/conta" end activeClassName={styles.active}>
+            <MinhasFotos />
+              {mobile && 'Minhas Fotos'}
+          </NavLink>
+          <NavLink to="/conta/estatisticas" activeClassName={styles.active}>
+            <Estatísticas/>
+              {mobile && 'Estatísticas'}
+          </NavLink>
+          <NavLink to="/conta/postar" activeClassName={styles.active}>
+            <AdicionarFotos />
+            {mobile && 'Adicionar Fotos'}        
+          </NavLink>
+
+          <button onClick={userLogout}>
+            <Sair />  
+            {mobile && 'Sair'} 
+          </button>
+        </nav>
+      </>
   );
 }
 
--- dogs/src/Utils/regex.js
+++ dogs/src/Utils/regex.js
@@ -1,13 +1,17 @@
 export const types = {
   email: {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    message: 'Preencha um email válido',
+    message: 'Preencha um email válido.',
   },
   password: {
     regex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/,
     message: 
     'A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 dígito.' + 
-    'Com no mínimo 8 caracteres'
+    'Com no mínimo 8 caracteres.'
+  },
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize números apenas.'
   }
 };
 
