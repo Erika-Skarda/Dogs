0389a6cf98361e35d3d5b90eaa60a7897bbb3c19
Erika Skarda <erikaskarda@yahoo.com.br>
2021-07-09 20:30:38 -0300

    Feat: Style Comment

--- dogs/src/Components/Photo/PhotoComments/PhotoComments.js
+++ dogs/src/Components/Photo/PhotoComments/PhotoComments.js
@@ -1,15 +1,20 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useRef, useEffect } from 'react';
 import  { useUser } from '../../../UseContext/UserContext';
 import PhotoCommentsForm from '../PhotoCommentsForm/PhotoCommentsForm';
 import styles from './PhotoComments.module.css';
 
 const PhotoComments = (props) => {
-  const [comments, setComments] = useState(() => props.comments)
+  const [comments, setComments] = useState(() => props.comments);
+  const commentsSection = useRef(null);
   const { login } = useUser();
+
+  useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.scrollHeight;
+  }, [comments]);
 
   return (
     <Fragment>
-      <ul className={styles.comments}>
+      <ul ref={commentsSection} className={styles.comments}>
         {comments.map((comment) => (
           <li key={comment.comment_ID}>
             <b>{comment.comment_author}: </b>
--- dogs/src/Components/Photo/PhotoCommentsForm/PhotoCommentsForm.js
+++ dogs/src/Components/Photo/PhotoCommentsForm/PhotoCommentsForm.js
@@ -3,7 +3,7 @@ import useFetch from '../../../Hooks/useFetch';
 import { COMMENT_POST } from '../../../Api/api';
 import Error from '../../Helper/Error';
 import { ReactComponent as Enviar} from '../../../Assets/enviar.svg';
-
+import styles from './PhotoCommentsForm.module.css';
 
 const PhotoCommentsForm = ({ id, setComments }) => {
   const [comment, setComment] = useState('');
@@ -13,22 +13,22 @@ const PhotoCommentsForm = ({ id, setComments }) => {
     e.preventDefault();
     const { url, options } = COMMENT_POST(id, { comment });
     const { response, json } = await request(url, options);
-    console.log(json)
     if (response.ok) {
       setComment('');
       setComments((comments) => [...comments, json]);
     }
   }
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id="comment"
         name="comment"
         placeholder="Comente..."
         value={comment} 
         onChange={({ target }) => setComment(target.value)}
       />
-      <button><Enviar /></button>
+      <button className={styles.button}><Enviar /></button>
       <Error error={error} />
     </form>
   );
