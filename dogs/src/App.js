4d2e268d651325996ecb1473eca72aa59476f383
Erika Skarda <erikaskarda@yahoo.com.br>
2021-07-20 19:28:49 -0300

    Feat: Perfil Usuário

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -5,6 +5,7 @@ import { UserStorage } from './UseContext/UserContext';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import User from './Components/User/User';
+import UserProfile from './Components/User/UserProfile/UserProfile'
 import Photo from './Components/Photo/Photo/Photo';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Home from './Pages/Home/Home';
@@ -22,6 +23,7 @@ function App() {
               <Route path='login/*' element={<Login />} />
               <ProtectedRoute path='conta/*' element={<User />} />
               <Route path='foto/:id' element={<Photo />} />
+              <Route path='perfil/:user' element={<UserProfile />} />
             </Routes>
           <Footer /> 
         </UserStorage>
